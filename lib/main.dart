4611611a77ea6d60bddaf588ddceff05bc083f06
Luis Flores <lf016158@gmail.com>
2021-11-25 18:01:49 -0600

    se crearon 2 metodos y un DropdownButton

--- lib/main.dart
+++ lib/main.dart
@@ -14,8 +14,8 @@ class MyCalculator extends StatefulWidget {
 class MyCalculatorState extends State<MyCalculator> {
   //Declaracion de Variables
   double _numberForm = 0;
-  String _startMeasure = "";
-  String _convertedMeasure = "";
+  String? _startMeasure;
+  String? _convertedMeasure;
   String _resultMessage = "";
   double _result = 0;
 
@@ -65,12 +65,40 @@ class MyCalculatorState extends State<MyCalculator> {
                 }).toList(),
                 onChanged: (value) {
                   /* Continuara... */
+                  onStartedMeasureChanged(value.toString());
                 },
               ),
+              DropdownButton(
+                isExpanded: true,
+                value: _convertedMeasure,
+                items: _measures.map((String value) {
+                  return DropdownMenuItem<String>(
+                    value: value,
+                    child: Text(value),
+                  );
+                }).toList(),
+                onChanged: (value) {
+                  /* Continuara... */
+                  onStartedMeasureChanged(value.toString());
+                },
+              ),
+              ElevatedButton(onPressed: (){},child: Text("Convertir"),)
             ],
           ),
         ),
       ),
     );
+  }
+
+  void onConvertedMeasureChanged(String value) {
+    setState(() {
+      _convertedMeasure = value;
+    });
+  }
+
+  void onStartedMeasureChanged(String value) {
+    setState(() {
+      _convertedMeasure = value;
+    });
   }
 }
